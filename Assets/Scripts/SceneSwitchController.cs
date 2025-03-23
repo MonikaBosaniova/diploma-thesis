1e07e08797e219bc1f155f2e48baaa670c7783fc
Monika Bošániová <monika.bosaniova@gmail.com>
2025-03-23 14:44:59 +0100

    main menu scene with scene loading and buttons

---
+++ Assets/Scripts/SceneSwitchController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadSceneByChildIndex(Transform child)
+    {
+        SceneManager.LoadScene(child.GetSiblingIndex() + 1);
+    }
+}
