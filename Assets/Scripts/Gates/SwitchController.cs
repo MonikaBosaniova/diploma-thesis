ac6d7e617f55c53de0c3fc7fc9fc1595724c134f
Monika Bošániová <monika.bosaniova@gmail.com>
2025-07-03 10:11:13 +0200

    gates can be set default value before start

--- Assets/Scripts/Gates/SwitchController.cs
+++ Assets/Scripts/Gates/SwitchController.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 
 namespace Gates
 {
     public class SwitchController : NodeController
     {
-        
+        [Header("SET BEFORE PLAYING")]
+        public bool InputValue = false;
+
+        private void Awake()
+        {
+            Value = InputValue;
+        }
     }
 }
