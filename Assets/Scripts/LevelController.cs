5bb80f2f1ef0e5298d86dba7a0cb86f070660acc
Monika Bošániová <monika.bosaniova@gmail.com>
2026-01-25 18:41:32 +0100

    tweening, presentation

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,23 +1,34 @@
 using System;
 using System.Collections;
+using DG.Tweening;
 using DialogueSystem;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    
+    [SerializeField] private float cameraXPos = 0;
     protected internal bool IsCompleted = false;
     protected event Action OnLevelStarted;
     protected internal event Action OnLevelEnded;
 
     public virtual void Init()
     {
-        DialogueSequenceController ds = transform.GetComponent<DialogueSequenceController>();
-        if (ds != null && ds.GetType() != typeof(EndDialogueSequenceController))
+        Transform camera = Camera.main.transform;
+        if (camera != null && !Mathf.Approximately(cameraXPos, camera.position.x))
         {
-            UIManager.Instance.ShowDialogWindowUI(ds.dialogueSequence);
-            //Debug.Log("Show DIALOGUE");
+            if(transform.GetSiblingIndex() != 0)
+                camera.DOMoveX(cameraXPos, 0.8f).OnComplete(SetupDialogueSequence);
+            else
+            {
+                camera.position = new Vector3(0,10,0);
+                SetupDialogueSequence();
+            }
         }
+        else
+        {
+            SetupDialogueSequence();
+        }
+        
     }
 
     public virtual void Close()
@@ -65,6 +76,16 @@ public class LevelController : MonoBehaviour
         ShowEndDialogueSequence();
         yield return new WaitForSeconds(.5f);
         HideEndDialogueSequence();
+    }
+
+    private void SetupDialogueSequence()
+    {
+        DialogueSequenceController ds = transform.GetComponent<DialogueSequenceController>();
+        if (ds != null && ds.GetType() != typeof(EndDialogueSequenceController))
+        {
+            UIManager.Instance.ShowDialogWindowUI(ds.dialogueSequence);
+            //Debug.Log("Show DIALOGUE");
+        }
     }
     
 }
