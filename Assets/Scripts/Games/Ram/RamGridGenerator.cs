f7383ca7a64205b9b26c348f4e9826d1e8ffc228
MonikaBosaniova <monika.bosaniova@gmail.com>
2026-02-07 12:32:30 +0100

    fixing bulding error

--- Assets/Scripts/Games/Ram/RamGridGenerator.cs
+++ Assets/Scripts/Games/Ram/RamGridGenerator.cs
@@ -1,4 +1,3 @@
-#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
@@ -7,43 +6,44 @@ namespace Games.Ram
 {
     public class RamGridGenerator : MonoBehaviour
     {
-        public GameObject cubePrefab;
-        public int x = 3, y = 3, z = 3;
-        public float spacing = 1f;
+        #if UNITY_EDITOR
+            public GameObject cubePrefab;
+            public int x = 3, y = 3, z = 3;
+            public float spacing = 1f;
 
-        [ContextMenu("Generate Grid")]
-        public void Generate()
-        {
-            Debug.Log("Generate Grid called");
-
-            if (cubePrefab == null)
+            [ContextMenu("Generate Grid")]
+            public void Generate()
             {
-                Debug.LogError("Cube prefab is NULL");
-                return;
+                Debug.Log("Generate Grid called");
+
+                if (cubePrefab == null)
+                {
+                    Debug.LogError("Cube prefab is NULL");
+                    return;
+                }
+
+                while (transform.childCount > 0)
+                {
+                    DestroyImmediate(transform.GetChild(0).gameObject);
+                }
+
+                for (int i = 0; i < x; i++)
+                for (int j = 0; j < y; j++)
+                for (int k = 0; k < z; k++)
+                {
+                    Vector3 pos = new Vector3(i, j, k) * spacing + transform.position;
+
+                    GameObject cube = (GameObject)PrefabUtility.InstantiatePrefab(cubePrefab);
+                    cube.transform.position = pos;
+                    cube.transform.rotation = Quaternion.identity;
+                    cube.transform.SetParent(transform);
+
+                    Undo.RegisterCreatedObjectUndo(cube, "Create Cube");
+                }
+
+                EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
             }
-
-            while (transform.childCount > 0)
-            {
-                DestroyImmediate(transform.GetChild(0).gameObject);
-            }
-
-            for (int i = 0; i < x; i++)
-            for (int j = 0; j < y; j++)
-            for (int k = 0; k < z; k++)
-            {
-                Vector3 pos = new Vector3(i, j, k) * spacing + transform.position;
-
-                GameObject cube = (GameObject)PrefabUtility.InstantiatePrefab(cubePrefab);
-                cube.transform.position = pos;
-                cube.transform.rotation = Quaternion.identity;
-                cube.transform.SetParent(transform);
-
-                Undo.RegisterCreatedObjectUndo(cube, "Create Cube");
-            }
-
-            EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
-        }
+        #endif
     }
 }
-#endif
     
