50883996e92b6b08f917a0d06ad61fc636b5eba6
MonikaBosaniova <monika.bosaniova@gmail.com>
2026-02-15 23:04:03 +0100

    reg data

---
+++ Assets/Scripts/Games/CPU/RegData.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+namespace Games.CPU
+{
+    public class RegData : MonoBehaviour
+    {
+        internal bool snapped = false;
+        internal float value;
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
