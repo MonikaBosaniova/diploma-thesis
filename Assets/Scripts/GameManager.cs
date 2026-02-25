10572d1baed6be1479f608722030b1312b6da410
MonikaBosaniova <monika.bosaniova@gmail.com>
2026-02-25 22:11:21 +0100

    vytvorenie scriptu na zrychlenie vypinania levelov. cooling presentacia a avatar predany

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -15,23 +16,23 @@ namespace GameStateMachine
         [SerializeField] protected GameObject quizParent;
         [SerializeField] protected GameObject endParent;
         [SerializeField] protected GameObject nextButton;
-        
+
         [SerializeField] internal string nodeID;
         [SerializeField] internal int stars = 0;
         [SerializeField] internal float time = 0;
-        
+
         private void Start()
         {
             Initialization();
         }
-        
+
         public void ChangeState(GameState newState)
         {
             CurrentState?.Exit();
             CurrentState = newState;
             CurrentState.Enter();
         }
-        
+
         protected virtual void Initialization()
         {
             if (ProgressService.I != null)
@@ -43,7 +44,7 @@ namespace GameStateMachine
                     stars = 1;
                 }
             }
-            
+
             GameState tutorialState = tutorialParent.AddComponent<TutorialState>();
             tutorialState.Init(tutorialParent);
             GameState minigameState = minigameParent.AddComponent<MinigameState>();
@@ -52,20 +53,16 @@ namespace GameStateMachine
             quizState.Init(quizParent);
             GameState endState = gameObject.AddComponent<EndState>();
             endState.Init(gameObject);
-            
-            tutorialState.OnStateComplete += () =>
-            {
-                ChangeState(minigameState);
- 
-            };
+
+            tutorialState.OnStateComplete += () => { ChangeState(minigameState); };
             minigameState.OnStateStart += () =>
             {
-                if(nextButton != null)
+                if (nextButton != null)
                     nextButton.SetActive(false);
             };
             minigameState.OnStateComplete += () => ChangeState(quizState);
             quizState.OnStateComplete += () => ChangeState(endState);
-            
+
             ChangeState(skipTutorial ? minigameState : tutorialState);
         }
 
@@ -73,5 +70,26 @@ namespace GameStateMachine
         {
             stars++;
         }
+
+        [ContextMenu("Hide All Levels")]
+        public void HideAllLevels()
+        {
+            List<LevelController> levels = new  List<LevelController>();
+            var tutorialLevels = tutorialParent.GetComponentsInChildren<LevelController>().ToList();
+            var  minigameLevels = minigameParent.GetComponentsInChildren<LevelController>().ToList();
+            
+            levels.AddRange(tutorialLevels);
+            levels.AddRange(minigameLevels);
+
+            foreach (var level in levels)
+            {
+                for (int i = 0; i < level.transform.childCount; i++)
+                {
+                    var child = level.transform.GetChild(i);
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
     }
 }
+
--- Assets/Scripts/Games/Cooling/CoolingGameManager.cs
+++ Assets/Scripts/Games/Cooling/CoolingGameManager.cs
@@ -7,34 +7,6 @@ namespace Games.Ram
 {
     public class CoolingGameManager : GameManager
     {
-        //[SerializeField] public List<GameObject> allPossibleGeneratedShapes;
 
-        // private void Awake()
-        // {
-        //     if(sliders != null) sliders.SetActive(false);
-        // }
-
-        // protected override void Initialization()
-        // {
-        //     base.Initialization();
-        //     var tutorialState = tutorialParent.GetComponent<TutorialState>();
-        //     var minigameState = minigameParent.GetComponent<MinigameState>();
-        //     var quizState = quizParent.GetComponent<QuizState>();
-        //
-        //     tutorialState.OnStateStart += () =>
-        //     {
-        //         if (sliders != null) sliders.SetActive(false);
-        //     };
-        //
-        //     minigameState.OnStateStart += () =>
-        //     {
-        //         if (sliders != null) sliders.SetActive(true);
-        //     };
-        //
-        //     quizState.OnStateStart += () =>
-        //     {
-        //         if (sliders != null) sliders.SetActive(false);
-        //     };
-        // }
     }
 }
