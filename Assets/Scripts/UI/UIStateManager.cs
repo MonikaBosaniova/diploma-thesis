38e4219d787347c0e3001cfc5e37e98f6077854d
Monika Bošániová <monika.bosaniova@gmail.com>
2026-03-28 13:07:29 +0100

    fixed localization in menu

--- Assets/Scripts/UI/UIStateManager.cs
+++ Assets/Scripts/UI/UIStateManager.cs
@@ -8,15 +8,16 @@ public class UIStateManager : MonoBehaviour
     [SerializeField] private Transform frontPCPanel;
 
     private GameObject menuStateParent;
-    private GameObject skillTreeParent;
+    [SerializeField] private RectTransform skillTreeBackButton;
     
     private float skillTreeHiddenPositionX;
+    private float skillTreeBackButtonHiddenPositionX;
     private float frontPanelStartPositionZ;
     private void Start()
     {
         menuStateParent = transform.GetChild(0).gameObject;
-        skillTreeParent = transform.GetChild(1).gameObject;
         skillTreeHiddenPositionX = skillTree.anchoredPosition.x;
+        skillTreeBackButtonHiddenPositionX = skillTreeBackButton.anchoredPosition.x;
         frontPanelStartPositionZ = frontPCPanel.localPosition.z;
         if (ProgressService.I.OpenSkillTree)
         {
@@ -35,15 +36,12 @@ public class UIStateManager : MonoBehaviour
         {
             case UIStates.None:
                 menuStateParent.SetActive(false);
-                skillTreeParent.SetActive(false);
                 break;
             case UIStates.Menu:
                 menuStateParent.SetActive(true);
-                skillTreeParent.SetActive(false);
                 break;
             case UIStates.SkillTree:
                 menuStateParent.SetActive(false);
-                skillTreeParent.SetActive(true);
                 break;
         }
     }
@@ -51,22 +49,16 @@ public class UIStateManager : MonoBehaviour
     public void ChangeToMenu()
     {
         frontPCPanel.DOLocalMoveZ(frontPanelStartPositionZ, .8f);
+        skillTreeBackButton.DOAnchorPosX(skillTreeBackButtonHiddenPositionX, .33f);
         skillTree.DOAnchorPosX(skillTreeHiddenPositionX, .33f).OnComplete(() =>UpdateState(UIStates.Menu));
     }
     
     public void ChangeToSkillTree(bool tween = true)
     {
         UpdateState(UIStates.SkillTree);
-        if (tween)
-        {
-            skillTree.DOAnchorPosX(0, .33f);
-            frontPCPanel.DOLocalMoveZ(-1f, .8f);
-        }
-        else
-        {
-            skillTree.DOAnchorPosX(0, 0);
-            frontPCPanel.DOLocalMoveZ(-1f, 0);
-        }
+        skillTreeBackButton.DOAnchorPosX(-skillTreeBackButtonHiddenPositionX, .33f);
+        skillTree.DOAnchorPosX(0, tween ? .33f : 0.0f);
+        frontPCPanel.DOLocalMoveZ(-1f, tween ? .8f : 0.0f);
     }
 
 }
